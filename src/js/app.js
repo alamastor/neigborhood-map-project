97e1c5cba05e67118733019930582e30f47110dd
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-03-07 12:38:34 +1100

    feat: Add results list

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,11 @@
-function initMap() {
+onload = function() {
     'use strict';
+    function ViewModel() {
+        var self = this;
+        self.places = ko.observableArray();
+    }
+    var viewModel = new ViewModel();
+
     // Create a map object and specify the DOM element for display.
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -37.8647, lng: 144.9696},
@@ -10,21 +16,32 @@ function initMap() {
     // TODO: handle map fail
     map.setTilt(45);
 
-    var home = new google.maps.LatLng(-37.8, 144.9);
+    var home = new google.maps.LatLng(-37.867556, 144.980302);
     var request = {
         location: home,
-        radius: 50000,
-        type: 'store'
+        radius: 3000,
+        type: 'bicycle_store',
     };
 
     var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, gotPlaces)
+    service.nearbySearch(request, gotPlaces);
 
     function gotPlaces(results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
+            console.log(results.length);
             for (var i = 0; i < results.length; i++) {
                 createMarker(results[i]);
+                viewModel.places.push(results[i]);
             }
+            viewModel.places.sort(function(a, b) {
+                if (a.name < b.name) {
+                    return -1;
+                }
+                if (a.name > b.name) {
+                    return 1;
+                }
+                return 0;
+            });
         } else {
             // TODO: handle request fail
         }
@@ -44,7 +61,9 @@ function initMap() {
             infowindow.open(map, marker);
         });
     }
+
+
+    ko.applyBindings(viewModel);
 }
 
-onload = initMap;
 
