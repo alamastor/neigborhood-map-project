5e4c4f6f5ee37a0772754d721844c86928252ac4
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-03-10 00:09:51 +1100

    refactor: Clean up experimental code

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,14 @@
 $(function() {
     'use strict';
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -37.8647, lng: 144.9696},
+        scrollwheel: true,
+        zoom: 14,
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+    });
+    // TODO: handle map fail
+
     function ViewModel() {
         var self = this;
         self.places = ko.observableArray();
@@ -41,47 +50,8 @@ $(function() {
 
     }
     var viewModel = new ViewModel();
+    ko.applyBindings(viewModel);
 
-    // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -37.8647, lng: 144.9696},
-        scrollwheel: true,
-        zoom: 14,
-        mapTypeId: google.maps.MapTypeId.TERRAIN,
-    });
-    // TODO: handle map fail
-
-    var home = new google.maps.LatLng(-37.867556, 144.980302);
-    var request = {
-        location: home,
-        radius: 3000,
-        type: 'bicycle_store',
-    };
-
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, gotPlaces);
-
-    function gotPlaces(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            console.log(results.length);
-            for (var i = 0; i < results.length; i++) {
-                results[i].suburb = results[i].vicinity.split(',')[1].trim()
-                createMarker(results[i]);
-                viewModel.places.push(results[i]);
-            }
-            viewModel.places.sort(function(a, b) {
-                if (a.name < b.name) {
-                    return -1;
-                }
-                if (a.name > b.name) {
-                    return 1;
-                }
-                return 0;
-            });
-        } else {
-            // TODO: handle request fail
-        }
-    }
 
     function createMarker(place)
     {
@@ -108,18 +78,50 @@ $(function() {
         });
     }
 
-    // Foursquare tests
-    var url = 'https://api.foursquare.com/v2/venues/search';
-    var data = {
-        client_id: fourSquareTokens.clientId,
-        client_secret: fourSquareTokens.clientSecret,
-        ll: '-37.8647,144.9696',
-        query: 'bicycle_store',
-        v: 20160310,
+    /**
+     * API requests
+     */
+
+    // Google places
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: new google.maps.LatLng(-37.867556, 144.980302),
+        radius: 3000,
+        type: 'bicycle_store',
+    }, gotPlaces);
+
+    function gotPlaces(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            console.log(results);
+            for (var i = 0; i < results.length; i++) {
+                results[i].suburb = results[i].vicinity.split(',')[1].trim()
+                createMarker(results[i]);
+                viewModel.places.push(results[i]);
+            }
+            viewModel.places.sort(function(a, b) {
+                if (a.name < b.name) {
+                    return -1;
+                }
+                if (a.name > b.name) {
+                    return 1;
+                }
+                return 0;
+            });
+        } else {
+            // TODO: handle request fail
+        }
     }
-    var request = $.ajax(url, {
+
+    // Foursquare search
+    var request = $.ajax('https://api.foursquare.com/v2/venues/search', {
         dataType: 'json',
-        data: data,
+        data: {
+            client_id: fourSquareTokens.clientId,
+            client_secret: fourSquareTokens.clientSecret,
+            ll: '-37.8647,144.9696',
+            query: 'bicycle',
+            v: 20160310,
+        },
     });
     request.done(function(msg) {
         console.log('done');
@@ -131,16 +133,23 @@ $(function() {
         console.log(textStatus);
     });
 
+
+    // Yelp API
+
+    // OAuth functions courtesy of MarkN from Udacity forums
+    // https://discussions.udacity.com/t/how-to-make-ajax-request-to-yelp-api/13699/5
+    // and Marco Bettiolo's Javascript OAuth Signature Generator
+    // https://github.com/bettiolo/oauth-signature-js
+
     /**
-     *  * Generates a random number and returns it as a string for OAuthentication
-     *   * @return {string}
-     *    */
+     ** Generates a random number and returns it as a string for OAuthentication
+     ** @return {string}
+     **/
     function nonce_generate() {
         return (Math.floor(Math.random() * 1e12).toString());
     }
 
-    //var yelp_url = 'https://api.yelp.com/v2/search' + self.selected_place().Yelp.business_id;
-    var yelp_url = 'https://api.yelp.com/v2/business/yelp-san-francisco';
+    var yelp_url = 'https://api.yelp.com/v2/search';
 
     var parameters = {
         oauth_consumer_key: yelpTokens.consumerKey,
@@ -149,22 +158,28 @@ $(function() {
         oauth_timestamp: Math.floor(Date.now()/1000),
         oauth_signature_method: 'HMAC-SHA1',
         oauth_version : '1.0',
+        term: 'bicycle_store',
+        cll: '-37.8647,144.9696',
+        location: 'St Kilda',
         callback: 'cb'              // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
     };
 
-    var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, yelpTokens.consumerSecret, yelpTokens.tokenSecret);
-    parameters.oauth_signature = encodedSignature;
+    parameters.oauth_signature = oauthSignature.generate(
+        'GET',
+        yelp_url,
+        parameters,
+        yelpTokens.consumerSecret,
+        yelpTokens.tokenSecret
+    );
 
-    var settings = {
+    // Send the API reqest
+    var request = $.ajax({
         url: yelp_url,
         data: parameters,
         cache: true,                // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
         dataType: 'jsonp',
-    };
-    // Send AJAX query via jQuery library.
-    var request = $.ajax(settings);
+    });
     request.done(function(msg) {
-        console.log('done');
         console.log(msg);
     });
     request.fail(function(jqXHR, textStatus) {
@@ -176,7 +191,5 @@ $(function() {
         console.log('GOT CBb');
     }
 
-    ko.applyBindings(viewModel);
 });
-
 
