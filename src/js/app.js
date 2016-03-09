bb95ae24bc924ca57ae09bee8ef9a4c71a2ebaef
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-03-09 22:04:45 +1100

    feat: Yelp tests working

--- src/js/app.js
+++ src/js/app.js
@@ -131,6 +131,51 @@ $(function() {
         console.log(textStatus);
     });
 
+    /**
+     *  * Generates a random number and returns it as a string for OAuthentication
+     *   * @return {string}
+     *    */
+    function nonce_generate() {
+        return (Math.floor(Math.random() * 1e12).toString());
+    }
+
+    //var yelp_url = 'https://api.yelp.com/v2/search' + self.selected_place().Yelp.business_id;
+    var yelp_url = 'https://api.yelp.com/v2/business/yelp-san-francisco';
+
+    var parameters = {
+        oauth_consumer_key: yelpTokens.consumerKey,
+        oauth_token: yelpTokens.token,
+        oauth_nonce: nonce_generate(),
+        oauth_timestamp: Math.floor(Date.now()/1000),
+        oauth_signature_method: 'HMAC-SHA1',
+        oauth_version : '1.0',
+        callback: 'cb'              // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
+    };
+
+    var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, yelpTokens.consumerSecret, yelpTokens.tokenSecret);
+    parameters.oauth_signature = encodedSignature;
+
+    var settings = {
+        url: yelp_url,
+        data: parameters,
+        cache: true,                // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
+        dataType: 'jsonp',
+    };
+    // Send AJAX query via jQuery library.
+    var request = $.ajax(settings);
+    request.done(function(msg) {
+        console.log('done');
+        console.log(msg);
+    });
+    request.fail(function(jqXHR, textStatus) {
+        console.log('failed');
+        console.log(jqXHR);
+        console.log(textStatus);
+    });
+    function cb(res) {
+        console.log('GOT CBb');
+    }
+
     ko.applyBindings(viewModel);
 });
 
