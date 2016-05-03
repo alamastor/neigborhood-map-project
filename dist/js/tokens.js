68477abf9726c973b92625f8ec92000c38f5bffc
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-05-03 21:52:51 +1000

    chore: Add built version to git

---
+++ dist/js/tokens.js
@@ -0,0 +1,1 @@
+define({fourSquareTokens:{owner:"Alistair McKelvie",clientId:"UPPJ3ZBOVQKZKZG5CBZYTMPMRNBJVHQI414NZIENADTLL1KW",clientSecret:"FOGFSTJRKL15TCQE43HQJVZUW5OCGJ4WVOZHX10NCEPM5DVL"},yelpTokens:{consumerKey:"2Je3yUemKYTKsVjvReQWWw",consumerSecret:"e6ceMnm2-Ow-Rr7otPLQqEXtDpk",token:"_bA2Z179iv_hLo9rAmnAUfv98XAYuqht",tokenSecret:"wKGDQTf3xgoX88U3GiBTg8OqV9o"}});
---
+++ src/js/tokens.js
@@ -0,0 +1,19 @@
+/**
+ * API tokens for Yelp and Foursquare. This is bad practice, on a real project
+ * they should be kept on the the server, and out of version control, with API
+ * calls also made server side.
+ */
+
+define({
+    fourSquareTokens: {
+        owner: 'Alistair McKelvie',
+        clientId: 'UPPJ3ZBOVQKZKZG5CBZYTMPMRNBJVHQI414NZIENADTLL1KW',
+        clientSecret: 'FOGFSTJRKL15TCQE43HQJVZUW5OCGJ4WVOZHX10NCEPM5DVL',
+    },
+    yelpTokens: {
+        consumerKey: '2Je3yUemKYTKsVjvReQWWw',
+        consumerSecret: 'e6ceMnm2-Ow-Rr7otPLQqEXtDpk',
+        token: '_bA2Z179iv_hLo9rAmnAUfv98XAYuqht',
+        tokenSecret: 'wKGDQTf3xgoX88U3GiBTg8OqV9o',
+    }
+});
