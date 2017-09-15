1f476246920a44e8a97459b6e5bb1440fdf74366
Alistair <alistair.mckelvie@gmail.com>
2017-09-15 17:43:07 +1000

    chore: Add google API key

--- src/js/main.js
+++ src/js/main.js
@@ -18,15 +18,15 @@ requirejs.config({
     }
 });
 
-define('google', ['async!https://maps.googleapis.com/maps/api/js?libraries=places'], function() {
+define('google', ['async!https://maps.googleapis.com/maps/api/js?key=AIzaSyBlllhsbJmuXJL9a1EMp8sax8CNUnBC_JM&libraries=places'], function () {
     return window.google;
 });
 
-define('oauthSignature', ['oauth-signature.min'], function() {
+define('oauthSignature', ['oauth-signature.min'], function () {
     return window.oauthSignature;
 });
 
-define('mapIcons', ['../../lib/js/map-icons.min'], function() {
+define('mapIcons', ['../../lib/js/map-icons.min'], function () {
     return {
         Marker: Marker,
         MAP_PIN: MAP_PIN,
@@ -34,34 +34,34 @@ define('mapIcons', ['../../lib/js/map-icons.min'], function() {
 });
 
 // Call bootstrap to force it to load and be used.
-requirejs(['bootstrap'], function() {});
+requirejs(['bootstrap'], function () { });
 
 requirejs(['jquery', '../app'],
-function(         $,     app) {
-    $(app.init());
-}, function(err) {
-    console.log(err);
-    if (err.requireModules.indexOf("async!https://maps.googleapis.com/maps/api/js?libraries=places_unnormalized2") !== -1) {
-        // Couldn't get google maps library, will have to disable most functionality.
-        // Alternative would be to fall back to local version of the library,
-        // but the library has other external depencies, which would also have to be
-        // stored locally, and whose URLs would need to altered to the local location.
-        requirejs.undef('google');
-        define('google', function() {
-            return {
-                load: 'fail',
-            };
-        });
-        requirejs.undef('mapIcons');
-        define('mapIcons', function() {
-            return {
-                load: 'fail',
-            };
-        });
-    }
-    requirejs.undef('../app');
-    requirejs(['jquery', '../app'],
-    function(         $,     app) {
-        // Original function that errored will be called again now
+    function ($, app) {
+        $(app.init());
+    }, function (err) {
+        console.log(err);
+        if (err.requireModules.indexOf("async!https://maps.googleapis.com/maps/api/js?libraries=places_unnormalized2") !== -1) {
+            // Couldn't get google maps library, will have to disable most functionality.
+            // Alternative would be to fall back to local version of the library,
+            // but the library has other external depencies, which would also have to be
+            // stored locally, and whose URLs would need to altered to the local location.
+            requirejs.undef('google');
+            define('google', function () {
+                return {
+                    load: 'fail',
+                };
+            });
+            requirejs.undef('mapIcons');
+            define('mapIcons', function () {
+                return {
+                    load: 'fail',
+                };
+            });
+        }
+        requirejs.undef('../app');
+        requirejs(['jquery', '../app'],
+            function ($, app) {
+                // Original function that errored will be called again now
+            });
     });
-});
